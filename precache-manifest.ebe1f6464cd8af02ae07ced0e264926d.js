8de8fab60b38c64302ef25f6012c311ab8fab3c5
Aneta Jagla <anett@poczta.fm>
2020-04-06 17:34:15 +0200

    Updates

---
+++ precache-manifest.ebe1f6464cd8af02ae07ced0e264926d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb2a02fd2a3436a6419cbf01f608eb6",
+    "url": "/Menu-restaurant/index.html"
+  },
+  {
+    "revision": "bc3b6ca3c7bafbed154a",
+    "url": "/Menu-restaurant/static/css/3.792babab.chunk.css"
+  },
+  {
+    "revision": "6363f262ccd9dd8785ee",
+    "url": "/Menu-restaurant/static/css/main.1cd9a4ed.chunk.css"
+  },
+  {
+    "revision": "bdd15ca8252cfff3fc3a",
+    "url": "/Menu-restaurant/static/js/2.b253eea0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Menu-restaurant/static/js/2.b253eea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3b6ca3c7bafbed154a",
+    "url": "/Menu-restaurant/static/js/3.479ddd0f.chunk.js"
+  },
+  {
+    "revision": "6363f262ccd9dd8785ee",
+    "url": "/Menu-restaurant/static/js/main.13686ea5.chunk.js"
+  },
+  {
+    "revision": "ae2f9727e94ebd8ae5e9",
+    "url": "/Menu-restaurant/static/js/runtime-main.ab0aae7c.js"
+  },
+  {
+    "revision": "701748e7fd7677e662160e5d6286030b",
+    "url": "/Menu-restaurant/static/media/chickenburger.701748e7.png"
+  }
+]);
